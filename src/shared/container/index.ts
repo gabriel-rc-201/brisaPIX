1239da5703d73daa3029727f10ccdc75de2a6e5b
gabriel_rc__ <ganriel201@gmail.com>
2022-03-31 16:33:12 -0300

    singleton do repositório das transações

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,6 +4,8 @@ import { IUserRepository } from "modules/user/repositories/IUsersRepository";
 import { UsersRepository } from "modules/user/repositories/implementations/UsersRepository";
 import { IChavesRepository } from "modules/chave/repositories/IChavesRepository";
 import { ChavesRepository } from "modules/chave/repositories/implementations/ChavesRepository";
+import { ITransacoesRepository } from "modules/transacao/repositories/ITransacoesRepository";
+import { TransacoesRepository } from "modules/transacao/repositories/implementations/TransacoesRepository";
 
 container.registerSingleton<IUserRepository>(
   "UsersRepository",
@@ -13,4 +15,9 @@ container.registerSingleton<IUserRepository>(
 container.registerSingleton<IChavesRepository>(
   "ChavesRepository",
   ChavesRepository
+);
+
+container.registerSingleton<ITransacoesRepository>(
+  "TransacoesRepository",
+  TransacoesRepository
 );
