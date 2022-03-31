6f859c407a669684dc88776ed7243b46f15bbe4f
gabriel_rc__ <ganriel201@gmail.com>
2022-03-31 10:47:20 -0300

    singleton do repositório das chaves

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IUserRepository } from "modules/user/repositories/IUsersRepository";
 import { UsersRepository } from "modules/user/repositories/implementations/UsersRepository";
+import { IChavesRepository } from "modules/chaves/repositories/IChavesRepository";
+import { ChavesRepository } from "modules/chaves/repositories/implementations/ChavesRepository";
 
 container.registerSingleton<IUserRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IChavesRepository>(
+  "ChavesRepository",
+  ChavesRepository
 );
