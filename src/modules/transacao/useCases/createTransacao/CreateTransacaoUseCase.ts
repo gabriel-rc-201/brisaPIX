c555bd12559a9d345f6babbb7cb3daafcee99c6a
gabriel_rc__ <ganriel201@gmail.com>
2022-04-01 10:18:05 -0300

    atualização de segurança para realização da transação

--- src/modules/transacao/useCases/createTransacao/CreateTransacaoUseCase.ts
+++ src/modules/transacao/useCases/createTransacao/CreateTransacaoUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 
 import { ITransacoesRepository } from "../../repositories/ITransacoesRepository";
 import { IChavesRepository } from "modules/chave/repositories/IChavesRepository";
+import { AppError } from "errors/AppError";
 
 interface IData {
   valor: number;
@@ -28,9 +29,21 @@ class CreateTransacaoUseCase {
       await this.chavesRepository.findChaveByKey(user_reciever_chave)
     ).user_id;
 
+    if (!user_sender_id)
+      throw new AppError(
+        "transação não autorizada, usuário não encontrado!!!",
+        401
+      );
+
     const user_reciever_id = (
       await this.chavesRepository.findChaveByKey(user_sender_chave)
     ).user_id;
+
+    if (!user_reciever_id)
+      throw new AppError(
+        "transação não autorizada, usuário não encontrado!!!",
+        401
+      );
 
     await this.transacoesRepository.create({
       valor,
